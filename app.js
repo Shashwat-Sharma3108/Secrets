bb68aec32dbe03942b1a4e7698457781de8ee507
Shashwat Sharma <70731419+Shashwat-Sharma3108@users.noreply.github.com>
2021-05-15 09:46:11 +0530

    Remove secret

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.use(session({
-    secret : "ThisIsaVeryLongSecret!",
+    secret : <Any String of Your Choice>,
     resave : false,
     saveUninitialized : false
 }));
